a443c2ad893440882cb4554093b776c44fb2921b
Lana Salah Shamaileh <shamaylehlana62@gmail.com>
2021-03-30 03:21:02 +0300

    edit

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
-# Lab: 02 - About Me, with a guessing game
+# **Lab: 02 - About Me and guessing game**
 
-***About Me - Guessing Game***
+## Project Name: ***About Me and guessing game***
 
-Author : Lana Salah Shamayleh
-
-## Links and Resources
-
-- [Submission's Link](https://canvas.instructure.com/courses/2625796/assignments/20846796)
-
-## Reflections and Comments
+>>- I build ***About Me and guessing game*** that let users know more fun information about me. The user must answer 4 questions about me, and each correct answer is granted with 1 points score.
+>>
+>>- After completing the game, the user can read more information about me from my site.
+>>
+>>**Author:Lana Salah Shamayleh**
+>>
+>>## Links and Resources
+>>
+>>- [Submission's Repository](https://github.com/LanaSShamayleh/About-Me.git)
+>>
+>>## Reflections and Comments
+>>
+>>- I added four questions to the guessing game.
+>>- Each correct answer is granted 1 points score, so the total out of 4.
--- js/app.js
+++ js/app.js
@@ -5,34 +5,34 @@
 
 var userName = prompt('Hey😉 What is your name ?');
 
-alert('Welcome ' + userName + ' .This My website 🙈 ,Lets we play a small game before to see the page!');
+alert('Welcome ' + userName + ' .This is My website 🙈 ,Lets we play a small game before to see this page!');
 
 let count = 0;
 
-var Q1= prompt('Are my age is 24 years old? You Answer must be (yes/no) or(y,n) ');
+var Q1= prompt('Are my age is 24 years old? Your Answer must be (yes/no) or(y,n) ');
 
 Qes1(Q1);
 
-var Q2 = prompt(' What is my major🤨? Do you think is the communication engineer 😋?You Answer must be (yes/no) or(y,n) ');
+var Q2 = prompt(' What is my major🤨? Do you think it is the communication engineer 😋?Your Answer must be (yes/no) or(y,n) ');
 
 Qes2(Q2);
 
 
-var Q3= prompt(' Do you think winter is my season 🌨 ?You Answer must be (yes/no) or(y,n) ');
+var Q3= prompt(' Do you think the winter is my season 🌨 ?Your Answer must be (yes/no) or(y,n) ');
 
 
 Qes3(Q3);
 
 
-var Q4= prompt('What is my Futur Goals🤨? Do you think to be Web Developer 😋? You Answer must be (yes/no) or(y,n) ');
+var Q4= prompt('What is My Future Goals🤨? Do you think to be a Web Developer 😋? Your Answer must be (yes/no) or(y,n) ');
 
 Qes4(Q4);
 
 
 if (count < 2)
-  alert('Thanks!' + userName + ' for play with us , You was a great Person and yeah you got \n' + count + '/4 hope see you again  ');
+  alert('Thanks' + userName + ', for play with us , You was a great person and  you got \n ' + count + '/4 hope see you again  ');
 else
-  alert('Thanks! ' + userName + ' for play with us , You was a great Person and yeah you got \n' + count + '/4,Congrats 😉');
+  alert('Thanks' + userName + ' ,for play with us , You was a great person and you got \n' + count + '/4 , Congrats 😉');
 
 
 //next
@@ -51,7 +51,7 @@ function Qes1(Q1) {
 
   }
   else {
-    alert('You Answer must be (yes/no) or(y,n)');
+    alert('Your Answer must be (yes/no) or(y,n)');
   }
 }
 // next
@@ -68,7 +68,7 @@ function Qes2(Q2) {
     alert('Your Anwer is not Correct , Try again 😎');
   }
   else {
-    alert('You Answer must be (yes/no) or(y,n) ');
+    alert('Your Answer must be (yes/no) or(y,n) ');
   }
 }
 
@@ -87,7 +87,7 @@ function Qes3(Q3) {
     alert('Your Anwer is not Correct , Try again');
   }
   else {
-    alert('You Answer must be (yes/no) or(y,n)  ');
+    alert('Your Answer must be (yes/no) or(y,n)  ');
   }
 }
 
@@ -104,6 +104,6 @@ function Qes4(Q4) {
 
     alert('Your Anwer is not Correct ');
   }else {
-    alert('You Answer must be (yes/no) or(y,n) ');
+    alert('Your Answer must be (yes/no) or(y,n) ');
   }
 }
