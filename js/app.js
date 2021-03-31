31b1bec535075a7cea527e8836bcf469c204d523
Lana Salah Shamaileh <shamaylehlana62@gmail.com>
2021-03-31 06:19:31 +0300

    Final Edit Assignment

--- js/app.js
+++ js/app.js
@@ -1,27 +1,53 @@
-/* eslint-disable no-var */
-/* eslint-disable new-cap */
-
 'use strict';
+
+let userName = prompt('Hey😉 What is your name ?');
+
+let count = 0;
+
+let answer;
+
+alert('Welcome ' + userName + ' .This is My website 🙈 ,Lets we play a small game before to see this page!');
+
+//  First part - Prompt and Calling
+
+var q1 = prompt('Are my age is 24 years old? Your Answer must be (yes/no) or(y,n) ');
+qes1(q1);
+
+var q2 = prompt(' What is my major🤨? Do you think it is the communication engineer 😋?Your Answer must be (yes/no) or(y,n) ');
+qes2(q2);
+
+var q3 = prompt(' Do you think the winter is my season 🌨 ?Your Answer must be (yes/no) or(y,n) ');
+qes3(q3);
+
+var q4 = prompt('What is My Future Goals🤨? Do you think to be a Web Developer 😋? Your Answer must be (yes/no) or(y,n) ');
+qes4(q4);
+
+var q5 = prompt('Do You think that a writing is one of my hobbies? Your Answer must be (yes/no) or(y,n) ');
+qes5(q5);
+
+qes6();
+
+qes7();
+
+if (count < 4) {
+  alert(`Thanks ${userName} for play with us , You was a great person and  you got ${count}/7 hope see you again `);
+}
+else {
+  alert(`Thanks${userName} for play with us , You was a great person and you got ${count}/7, Congrats 😉`);
+}
 
 // First part - prompt and calling
 
-var userName = prompt('Hey😉 What is your name ?');
+function qes1(q1) {
 
-let count = 0;
-let answer;
-alert('Welcome ' + userName + ' .This is My website 🙈 ,Lets we play a small game before to see this page!');
-
-// First Function
-function Qes1(Q1) {
-
-  if (Q1.toLowerCase() === 'yes' || Q1.toLowerCase() === 'y') {
+  if (q1.toLowerCase() === 'yes' || q1.toLowerCase() === 'y') {
     alert('Your Anwer is Correct 😎');
 
     console.log('Your Anwer in question (1) is  Correct ');
 
     count++;
   }
-  else if (Q1.toLowerCase() === 'no' || Q1.toLowerCase() === 'n') {
+  else if (q1.toLowerCase() === 'no' || q1.toLowerCase() === 'n') {
     alert('Your Anwer is not Correct 😉');
 
   }
@@ -31,15 +57,15 @@ function Qes1(Q1) {
 }
 // next function
 
-function Qes2(Q2) {
-  if (Q2.toLowerCase() === 'yes' || Q2.toLowerCase() === 'y') {
+function qes2(q2) {
+  if (q2.toLowerCase() === 'yes' || q2.toLowerCase() === 'y') {
 
     alert('Your Anwer is  Correct 😉');
 
     console.log('Your Anwer in question (2) is  Correct ');
     count++;
   }
-  else if (Q2.toLowerCase() === 'no' || Q2.toLowerCase() === 'n') {
+  else if (q2.toLowerCase() === 'no' || q2.toLowerCase() === 'n') {
     alert('Your Anwer is not Correct , Try again 😎');
   }
   else {
@@ -49,8 +75,8 @@ function Qes2(Q2) {
 
 // next function
 
-function Qes3(Q3) {
-  if (Q3.toLowerCase() === 'yes' || Q3.toLowerCase() === 'y') {
+function qes3(q3) {
+  if (q3.toLowerCase() === 'yes' || q3.toLowerCase() === 'y') {
 
     alert('Your Anwer is  Correct 😎');
 
@@ -58,7 +84,7 @@ function Qes3(Q3) {
 
     count++;
   }
-  else if (Q3.toLowerCase() === 'no' || Q3.toLowerCase() === 'n') {
+  else if (q3.toLowerCase() === 'no' || q3.toLowerCase() === 'n') {
     alert('Your Anwer is not Correct , Try again');
   }
   else {
@@ -68,14 +94,14 @@ function Qes3(Q3) {
 
 // next function
 
-function Qes4(Q4) {
+function qes4(q4) {
 
-  if (Q4.toLowerCase() === 'yes' || Q4.toLowerCase() === 'y') {
+  if (q4.toLowerCase() === 'yes' || q4.toLowerCase() === 'y') {
     alert('Your Anwer is Correct  😎');
     console.log('Your Anwer in question (4) is  Correct');
     count++;
   }
-  else if (Q4.toLowerCase() === 'no' || Q4.toLowerCase() === 'n') {
+  else if (q4.toLowerCase() === 'no' || q4.toLowerCase() === 'n') {
 
     alert('Your Anwer is not Correct ');
   } else {
@@ -85,14 +111,14 @@ function Qes4(Q4) {
 
 // next function
 
-function Qes5(Q5) {
+function qes5(q5) {
 
-  if (Q5.toLowerCase() === 'yes' || Q5.toLowerCase() === 'y') {
+  if (q5.toLowerCase() === 'yes' || q5.toLowerCase() === 'y') {
     alert('Your Anwer is Correct  😎');
     console.log('Your Anwer in question (5) is  Correct ');
     count++;
   }
-  else if (Q5.toLowerCase() === 'no' || Q5.toLowerCase() === 'n') {
+  else if (q5.toLowerCase() === 'no' || q5.toLowerCase() === 'n') {
 
     alert('Your Anwer is not Correct ');
   } else {
@@ -102,14 +128,14 @@ function Qes5(Q5) {
 
 // next function
 
-function Qes6() {
+function qes6() {
 
-  var Array = ['try to guess my lucky number ( 1 and 10 )', 'If i would to change one my hobbies, which one would i change it?'];
+  var array = ['try to guess my lucky number ( 1 and 10 )', 'If i would to change one my hobbies, which one would i change it?'];
 
   let n;
   for (n = 0; n < 4; n++) {
 
-    answer = prompt('Hey' + userName + '! this is the last question,so ' + Array[0] + '.Note you have only ' + (4 - n) + 'attempts! ');
+    answer = prompt('Hey' + userName + '! this is the last question,so ' + array[0] + '.Note you have only ' + (4 - n) + 'attempts! ');
     switch (answer) {
     case '1':
     case '2':
@@ -135,7 +161,7 @@ function Qes6() {
 
     case '7':
       count++;
-      console.log('Your Anwer in question (9) is  Correct , we finished 😋');
+      console.log('Your Anwer in question (6) is  Correct ');
       n = 5;
       break;
     default:
@@ -147,9 +173,9 @@ function Qes6() {
 
 // next function
 
-function Qes7() {
-  var Q7Ans;
-  var Levels = ['code101', 'code102', 'code201', 'code301'];
+function qes7() {
+  var q7Ans;
+  var levels = ['code101', 'code102', 'code201', 'code301'];
   var correct = false;
   var k = 0;
 
@@ -157,56 +183,31 @@ function Qes7() {
 
     console.log(k, 'attempt');
 
-    Q7Ans = prompt('What are the Levels that I took in the Web Development Course ,you have 6 attempts to guess the correct answer\n Hint: The answer include the name of code 🙄 ');
+    q7Ans = prompt('What are the Levels that I took in the Web Development Course ,you have 6 attempts to guess the correct answer\n Hint: The answer include the name of code 🙄 ');
 
-    for (var j = 0; j < Levels.length; j++) {
+    for (var j = 0; j < levels.length; j++) {
 
 
-      if (Q7Ans.toLowerCase() === Levels[j]) {
+      if (q7Ans.toLowerCase() === levels[j]) {
 
-        alert(` Very Good 😉. This is a Correct Answer,See all correct answers about my levels \n ('  ${Levels} ') .`);
-        console.log('Your Anwer in question (8) is  Correct ');
+        alert(` Very Good 😉. This is a Correct Answer,See all correct answers about my levels \n ('  ${levels} ') .`);
+        console.log('Your Anwer in question (7) is  Correct, we finished 😋 ');
         correct = true;
         count++;
         break;
       }
+
+      if (correct) {
+        break;
+      }
+      alert('Wrong Answer ⁉, ' + (5 - k) + ' Attempt remain');
+      k++;
+
+
+      if (!correct)
+
+        alert(`you didn't guess,the correct answer its any one code of this list\n (' ${levels} ') ,hope see you again `);
+
     }
-    if (correct)
-      break;
-
-
-    alert('Wrong Answer ⁉, ' + (5 - k) + ' Attempt remain');
-    k++;
   }
-
-  if (!correct)
-    alert(`you didn't guess,the correct answer its any one code of this list\n (' ${Levels} ') ,hope see you again `);
 }
-
-//  Second part - Prompt and Calling
-
-var Q1 = prompt('Are my age is 24 years old? Your Answer must be (yes/no) or(y,n) ');
-Qes1(Q1);
-
-var Q2 = prompt(' What is my major🤨? Do you think it is the communication engineer 😋?Your Answer must be (yes/no) or(y,n) ');
-Qes2(Q2);
-
-var Q3 = prompt(' Do you think the winter is my season 🌨 ?Your Answer must be (yes/no) or(y,n) ');
-Qes3(Q3);
-
-var Q4 = prompt('What is My Future Goals🤨? Do you think to be a Web Developer 😋? Your Answer must be (yes/no) or(y,n) ');
-Qes4(Q4);
-
-var Q5 = prompt('Do You think that a writing is one of my hobbies? Your Answer must be (yes/no) or(y,n) ');
-Qes5(Q5);
-
-Qes6();
-
-Qes7();
-
-if (count < 4) {
-  alert(`Thanks ${userName} for play with us , You was a great person and  you got ${count}/7 hope see you again `);
-} else {
-  alert(`Thanks${userName} for play with us , You was a great person and you got ${count}/7, Congrats 😉`);
-}
-
