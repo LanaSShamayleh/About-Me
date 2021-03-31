d7909e54bc21affc8573946841a6494a53600bcb
Lana Salah Shamaileh <shamaylehlana62@gmail.com>
2021-03-31 06:43:53 +0300

    Final Edit Assignment

--- js/app.js
+++ js/app.js
@@ -8,35 +8,7 @@ let answer;
 
 alert('Welcome ' + userName + ' .This is My website 🙈 ,Lets we play a small game before to see this page!');
 
-//  First part - Prompt and Calling
-
-var q1 = prompt('Are my age is 24 years old? Your Answer must be (yes/no) or(y,n) ');
-qes1(q1);
-
-var q2 = prompt(' What is my major🤨? Do you think it is the communication engineer 😋?Your Answer must be (yes/no) or(y,n) ');
-qes2(q2);
-
-var q3 = prompt(' Do you think the winter is my season 🌨 ?Your Answer must be (yes/no) or(y,n) ');
-qes3(q3);
-
-var q4 = prompt('What is My Future Goals🤨? Do you think to be a Web Developer 😋? Your Answer must be (yes/no) or(y,n) ');
-qes4(q4);
-
-var q5 = prompt('Do You think that a writing is one of my hobbies? Your Answer must be (yes/no) or(y,n) ');
-qes5(q5);
-
-qes6();
-
-qes7();
-
-if (count < 4) {
-  alert(`Thanks ${userName} for play with us , You was a great person and  you got ${count}/7 hope see you again `);
-}
-else {
-  alert(`Thanks${userName} for play with us , You was a great person and you got ${count}/7, Congrats 😉`);
-}
-
-// First part - prompt and calling
+// First part - Functions
 
 function qes1(q1) {
 
@@ -181,7 +153,7 @@ function qes7() {
 
   while (k < 6) {
 
-    console.log(k, 'attempt');
+    //console.log(k, 'attempt');
 
     q7Ans = prompt('What are the Levels that I took in the Web Development Course ,you have 6 attempts to guess the correct answer\n Hint: The answer include the name of code 🙄 ');
 
@@ -196,18 +168,41 @@ function qes7() {
         count++;
         break;
       }
+    }if (correct)
+      break;
+    alert('Wrong Answer ⁉, ' + (5 - k) + ' Attempt remain');
+    k++;
 
-      if (correct) {
-        break;
-      }
-      alert('Wrong Answer ⁉, ' + (5 - k) + ' Attempt remain');
-      k++;
+  }if (!correct)
+    alert(`you didn't guess,the correct answer its any one code of this list\n (' ${levels} ') ,hope see you again `);
+
+}
 
 
-      if (!correct)
+//  Second part - Prompt and Calling
 
-        alert(`you didn't guess,the correct answer its any one code of this list\n (' ${levels} ') ,hope see you again `);
+var q1 = prompt('Are my age is 24 years old? Your Answer must be (yes/no) or(y,n) ');
+qes1(q1);
 
-    }
-  }
+var q2 = prompt(' What is my major🤨? Do you think it is the communication engineer 😋?Your Answer must be (yes/no) or(y,n) ');
+qes2(q2);
+
+var q3 = prompt(' Do you think the winter is my season 🌨 ?Your Answer must be (yes/no) or(y,n) ');
+qes3(q3);
+
+var q4 = prompt('What is My Future Goals🤨? Do you think to be a Web Developer 😋? Your Answer must be (yes/no) or(y,n) ');
+qes4(q4);
+
+var q5 = prompt('Do You think that a writing is one of my hobbies? Your Answer must be (yes/no) or(y,n) ');
+qes5(q5);
+
+qes6();
+
+qes7();
+
+if (count < 4) {
+  alert(`Thanks ${userName} for play with us , You was a great person and  you got ${count}/7 hope see you again `);
+}
+else {
+  alert(`Thanks${userName} for play with us , You was a great person and you got ${count}/7, Congrats 😉`);
 }
